1ee0a15c789687b3b7c5e67f01d7b8bf04d9fe82
Herm Martini <hermpheus@gmail.com>
2021-05-17 15:34:56 -0700

    Move dotfiles into subdirectory
    Removes need for exceptions in rsync

--- dot.sh
+++ dot.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-rsync --exclude ".git/" \
-    --exclude ".DS_Store" \
-    --exclude "dot.sh" \
-    --exclude ".macos" \
-    --exclude "README.md" \
-    -avh --no-perms . ~;
+rsync -avh --no-perms dots/. ~;
